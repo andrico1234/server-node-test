d205ba664669719f71517c381bc09dc12f22165a
Andrico <andrico1234@yahoo.co.uk>
2017-03-21 20:12:18 +0000

    add additional middleware functionality to express

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 'use strict';
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('file-system');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.originalUrl}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+
+        if (err) {
+            console.log('Unable to append log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
